7513ab8eea4d0004178442692f39fc046552fd89
John Doe <johndoe@example.com>
2021-02-01 12:02:37 +0700

    newJWT

--- web.js
+++ web.js
@@ -11,7 +11,7 @@ web.use(bodyparser.json());
 web.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-    res.header("Access-Control-Allow-Headers", "Content-Type");
+    res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, authorization ");
     next();
   });
 
