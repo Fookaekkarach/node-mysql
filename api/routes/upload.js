653ae3f61a540b83cca5874815d844a5b3971fdb
John Doe <johndoe@example.com>
2021-02-07 21:03:20 +0700

    new

--- api/routes/upload.js
+++ api/routes/upload.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('../../dbconn');
+const pool = require('../../dbconn'); 
 const mysql = require('mysql');
 
 const multer  = require('multer');
